9d7e3e1f8e09923dba7545a3300c0b35a6c8d30a
IlanRV <146100248+IlanRV@users.noreply.github.com>
2024-02-05 11:48:14 +0000

    task1 learning argv argcc

--- tasks/task.c
+++ tasks/task.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h> // for atoi
 
-int main() {
-    int num1, num2;
-
+int main(int argc, char *argv[]) {
+    
+    //int num1, num2;
+    if (argc != 3) {
+        printf("Usage: %s  <num1> <num2>\n", argv[0]);
+    }
+    
     // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+        //printf("Enter the first number: ");
+        //scanf("%d", &num1);
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    //printf("Enter the second number: ");
+    //scanf("%d", &num2);
 
     // Calculate and print the sum
     int sum = num1 + num2;
